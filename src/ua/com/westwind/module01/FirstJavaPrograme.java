d9082b59ba78ea08d2acdcc9e7d4857ec8730ce6
andriymartynyuk <andriy.itbox@gmail.com>
2016-05-24 12:08:47 +0300

    Initial commit of the project.

---
+++ src/ua/com/westwind/module01/FirstJavaPrograme.java
@@ -0,0 +1,7 @@
+package ua.com.westwind.module01;
+
+public class FirstJavaPrograme {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
