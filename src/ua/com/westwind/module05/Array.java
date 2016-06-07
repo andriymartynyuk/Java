8088302d7eecd64dfdf273c57664d27f8298806a
andriymartynyuk <andriy.itbox@gmail.com>
2016-06-08 00:49:35 +0300

    Min Max value of array

--- src/ua/com/westwind/module05/Array.java
+++ src/ua/com/westwind/module05/Array.java
@@ -1,22 +1,13 @@
 package ua.com.westwind.module05;
 
+import java.util.Arrays;
+
 public class Array {
     public static void main(String[] args) {
         int[] intArray = new int[]{10, 20, 30, 40, 50};
-
-        for (int i = 0; i < intArray.length; ++i) {
-            if (intArray[i] > intArray[i] + 1) {
-                int max = intArray[i];
-                System.out.println(max+"1");
-            }
-            else
-                {
-                int min = intArray[i];
-                    System.out.println(min+"2");
-                }
-
-        }
-
-
+        Arrays.sort(intArray);
+        int i = intArray.length-1;
+        System.out.println("Min value of array: "+intArray[0]);
+        System.out.println("Max velue of array: "+intArray[i]);
     }
 }
