1a34cd69b93aee1f2e9aaa8d5cf3156ce4b923ae
andriymartynyuk <andriy.itbox@gmail.com>
2016-06-08 17:56:52 +0300

    insertion sort

---
+++ src/ua/com/westwind/module05/InsertionSort.java
@@ -0,0 +1,27 @@
+package ua.com.westwind.module05;
+
+public class InsertionSort {
+    public static void main(String a[]){
+        int[] arr1 = {9,35,5,65,8,12,32,123};
+        int[] arr2 = doInsertionSort(arr1);
+        for(int i:arr2){
+            System.out.print(i);
+            System.out.print(", ");
+        }
+    }
+
+    public static int[] doInsertionSort(int[] input){
+
+        int temp;
+        for (int i = 1; i < input.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                if(input[j] < input[j-1]){
+                    temp = input[j];
+                    input[j] = input[j-1];
+                    input[j-1] = temp;
+                }
+            }
+        }
+        return input;
+    }
+}
